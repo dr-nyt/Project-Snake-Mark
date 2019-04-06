4c3840d44cc166e401222fbd97630c1b7d0c0d1e
dr_nyt <unconfigured@null.spigotmc.org>
2019-04-06 22:13:40 +0400

    Initial commit

---
+++ Assets/Scripts/StaticEnemy.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] waypoints;
+    private Vector3 targetPosition;
+
+    [SerializeField]
+    [Range(0, 1f)]
+    private float moveSpeed = 0.2f;
+
+    private int waypointIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        waypointIndex = 0;
+        targetPosition = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, .5f * moveSpeed);
+        if (Vector3.Distance(transform.position, targetPosition) < .25f)
+        {
+            if (waypointIndex + 1 >= waypoints.Length)
+            {
+                waypointIndex = 0;
+            }
+            else
+            {
+                waypointIndex++;
+            }
+            targetPosition = waypoints[waypointIndex].position;
+        }
+    }
+}
