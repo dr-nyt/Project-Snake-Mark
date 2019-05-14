8137132c709bc0f46ec55b8e3c5e2240dac2076e
dr_nyt <unconfigured@null.spigotmc.org>
2019-05-14 07:02:56 +0400

    Unknown Changes

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+    private GameObject menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu = transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Cancel"))
+        {
+            menu.SetActive(!menu.activeSelf);
+        }
+    }
+}
